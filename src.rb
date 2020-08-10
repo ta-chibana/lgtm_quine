0eaf237ae7c4dc4f5ac8d905c2b09ea354b7a2f7
ta-chibana <tachibana.gmc@gmail.com>
2020-08-10 18:21:46 +0900

    Add transition

--- src.rb
+++ src.rb
@@ -1,7 +1,6 @@
 i = 0;
 eval$s=%w(
   s=%(
-    i=#{(i+1)%4};
     eval$s=%w(#{$s})*"";
   );
 
@@ -11,46 +10,51 @@ eval$s=%w(
     "sgO2ME;poF1=m,@XWI?<.eYg3DGA8XD/N9]e]93ZsJkFBqt+~dAX8w1O9a2eRsey0tIUwk5>UShe1y`M*9AOrX=[v^mP,VV;Ct.T*vIu4K_e}wqRrLn^Yws7LELA^o9ZiWJMcegE=lUh",
     "sgO2ME;poF1=m,@XWI?<.eYg3DGA8XDoloMVV3`>Ae-{,w>-;X|~CVeyYWS,762;VI>swMN5CNO}=@>J`o:Nzv*1|[KH;D=SCD?~upxiJLZJKhnXoG3F7uEWMOkw+ueY@G0@K|mWbcC`"
   ];
-
-  n = WS[i];
-  f = 0;
-  n.bytes { |m| f = f * 84 + (m - 8) % 85 };
-
-  st = 0;
-  ps = false;
-  sep = 38.chr;
-
-  g = -> l {
-    cc = l.count('1');
-    cs = '';
-    nst = 0;
-    ft = -> {
-      h = s.slice(st..-1);
-      nst = st + cc - s.size;
-      if (!ps);
-        s = '%q' + sep + s;
-        ps = true;
-      end;
-      t = s.slice(0, nst);
-      cs = h + t;
-    };
-    ff = -> {
-      cs = s.slice(st, cc);
-      nst = st + cc;
-    };
-    (st + cc > s.size) ? ft.call() : ff.call();
-    st = nst;
-    l.split('').reduce('') { |a, c|
-      a + ((c == '1') ? cs.slice!(0) : 32.chr)
-    }
-  };
-
   R = 19;
   C = 47;
   P = '1' * C;
-  4.times { puts(g[P]) };
-  R.times { |y|
-    puts(g[(0..C - 1).map { |x| f[x + y * C] }.join]);
-  };
-  puts(g[P], g[P], g[P], g[P[0..-2]] + sep);
+
+  print("\e[2J\e[1;1H");
+  WS.each { |ws|
+    n = ws;
+    f = 0;
+    n.bytes { |m| f = f * 84 + (m - 8) % 85 };
+
+    st = 0;
+    ps = false;
+    sep = 38.chr;
+
+    g = -> l {
+      cc = l.count('1');
+      cs = '';
+      nst = 0;
+      ft = -> {
+        h = s.slice(st..-1);
+        nst = st + cc - s.size;
+        if (!ps);
+          s = '%q' + sep + s;
+          ps = true;
+        end;
+        t = s.slice(0, nst);
+        cs = h + t;
+      };
+      ff = -> {
+        cs = s.slice(st, cc);
+        nst = st + cc;
+      };
+      (st + cc > s.size) ? ft.call() : ff.call();
+      st = nst;
+      l.split('').reduce('') { |a, c|
+        a + ((c == '1') ? cs.slice!(0) : 32.chr)
+      }
+    };
+
+    4.times { puts(g[P]) };
+    R.times { |y|
+      puts(g[(0..C - 1).map { |x| f[x + y * C] }.join]);
+    };
+    puts(g[P], g[P], g[P], g[P[0..-2]] + sep);
+    sleep(0.5);
+    print("\e[2J\e[1;1H");
+  }
 )*"";
